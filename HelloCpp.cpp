ddf0644343a5adf2806a771ff647d48c00e217e9
Alisher Alirakym <alirahimalisher@gmail.com>
2026-07-20 16:52:45 +0500

    Add battle loops and damage validation

--- HelloCpp.cpp
+++ HelloCpp.cpp
@@ -9,6 +9,7 @@ int main()
     int playerMana;
     int playerGold;
     int damage;
+    int round = 1;
 
     std::cout << "Enter player name: ";
     std::cin >> playerName;
@@ -25,25 +26,6 @@ int main()
     std::cout << "Enter player gold: ";
     std::cin >> playerGold;
 
-    std::cout << "Enter damage: ";
-    std::cin >> damage;
-
-    playerHealth = playerHealth - damage;
-
-    if (playerHealth <= 0)
-    {
-        playerHealth = 0;
-        std::cout << "\nStatus: Dead\n";
-    }
-    else if (playerHealth <= 25)
-    {
-        std::cout << "\nStatus: Critical\n";
-    }
-    else
-    {
-        std::cout << "\nStatus: Alive\n";
-    }
-
     if (playerMana >= 20)
     {
         std::cout << "\nMagic: Spell ready\n";
@@ -53,6 +35,50 @@ int main()
         std::cout << "\nMagic: Not enough mana!\n";
     }
 
+    std::cout << "\nBattle starts in:\n";
+
+    for (int i = 3; i >= 1; i--)
+    {
+        std::cout << i << "\n";
+    }
+
+    std::cout << "Fight!\n";
+
+    while (playerHealth > 0)
+    {
+        std::cout << "\n=== Round " << round << " ===\n";
+
+        
+        std::cout << "Enter damage: ";
+        std::cin >> damage;
+        while (damage <= 0)
+        {
+            std::cout << "Damage must be greater than 0. Enter again: ";
+            std::cin >> damage;
+        }
+
+        playerHealth = playerHealth - damage;
+
+        if (playerHealth <= 0)
+        {
+            playerHealth = 0;
+            std::cout << "\nStatus: Dead\n";
+        }
+        else if (playerHealth <= 25)
+        {
+            std::cout << "\nStatus: Critical\n";
+        }
+        else
+        {
+            std::cout << "\nStatus: Alive\n";
+        }
+        
+        std::cout << "Current health: " << playerHealth << "\n";
+
+        round = round + 1;
+    }
+
+    
     std::cout << "\n === Character Profile === \n";
     std::cout <<" Name: " << playerName << '\n';
     std::cout << " Level: " << playerLevel << '\n';
